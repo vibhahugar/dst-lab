970c81e15e384b9c3025b01ae236e175727cd7f0
vibhahugar <117517944+vibhahugar@users.noreply.github.com>
2022-12-17 12:45:00 +0530

    Add files via upload

--- linklist.c
+++ linklist.c
@@ -10,13 +10,16 @@ typedef struct NODE Node;
 Node *start=NULL,*new,*curr;
 void create();
 void display();
+void insert_beg();
+void insert_end();
+void insert_pos();
 void main()
 {
 
     int choice;
     while(1)
   {
-      printf("\n 1.CREATE\n 2.DISPLAY\n 3.EXIT\n");
+      printf("\n 1.CREATE\n 2.DISPLAY\n 3.INSERT at the beginning\n 4.INSERT at the end\n 5.INSERT at given position \n 6.EXIT\n");
       printf("\n Enter your choice \n");
       scanf("%d",&choice);
       switch(choice)
@@ -27,56 +30,132 @@ void main()
             break;
      case 2:display();
             break;
-     case 3:exit(0);
+     case 3:insert_beg();
+            break;
+     case 4:insert_end();
+            break;
+     case 5:insert_pos();
+            break;
+     case 6:exit(0);
             break;
      default:printf("\n Invalid choice \n");
         }
 }
 getch();
 }
+
 void create()
 {
-char ch;
-    start=(Node*)malloc(sizeof(Node));
-    curr=start;
-    printf("\n Enter element\n");
-    scanf("%d",&start->data);
+    int choice;
+    Node *new, *curr;
+    start = (Node*)malloc(sizeof(Node));
+    curr = start;
+    printf("Enter element:\n");
+    scanf("%d", &start->data);
     while(1)
     {
-
-        printf(" \n Do you want to add another elements (Y/N) \n");
-        scanf("%c",&ch);
-        if(ch=='y'||ch=='Y')
+        printf("Do you want to add an element? Enter 0 for no\n");
+        scanf("%d", &choice);
+        if(choice!=0)
         {
-
-            new=(Node*)malloc(sizeof(Node));
-            printf(" \n Enter element\n");
-            scanf("%d",&new->data);
-            curr->link=new;
-            new=curr;
+            new = (Node*)malloc(sizeof(Node));
+            printf("Please enter element:\n");
+            scanf("%d", &new->data);
+            curr->link = new;
+            curr = new;
         }
         else
         {
-
             curr->link=NULL;
             break;
         }
     }
 }
+
+
 void display()
 {
-
     Node *temp;
+    temp = start;
     if(start==NULL)
     {
-
-        printf(" \n Linked list is empty\n");
+        printf("Linked list is empty\n");
         return;
     }
-    temp=start;
     while(temp!=NULL)
     {
-        printf("%d\t",temp->data);
-        temp=temp->link;
+        printf("%d\t", temp->data);
+        temp= temp->link;
     }
+
+}
+
+void insert_beg()
+{
+new=(Node*)malloc(sizeof(Node));
+printf("Enter element\n");
+scanf("%d",&new->data);
+if(start==NULL)
+{
+start=new;
+new->link=NULL;
+return;
+}
+new->link=start;
+start=new;
+}
+
+void insert_end()
+{
+Node * temp;
+new=(Node*)malloc(sizeof(Node));
+printf("Enter element\n");
+scanf("%d",&new->data);
+if(start==NULL)
+{
+start=new;
+new->link=NULL;
+return;
+}
+temp=start;
+while(temp->link!=NULL)
+{
+temp=temp->link;
+}
+temp->link=new;
+new->link=NULL;
+}
+
+void insert_pos()
+{
+Node * temp;
+int pos,i=1;
+new=(Node*)malloc(sizeof(Node));
+printf("Enter element\n");
+scanf("%d",&new->data);
+printf("Enter pos\n");
+scanf("%d",&pos);
+if(pos==1)
+{
+new->link=start;
+start=new;
+return;
+}
+temp=start;
+while(i<(pos-1)&&temp!=NULL)
+{
+temp=temp->link;
+i++;
+}
+if(i==(pos-1))
+{
+new->link=temp->link;
+temp->link=new;
+return;
+}
+if(temp==NULL)
+{
+printf("Invalid position\n");
+}
+
 }
